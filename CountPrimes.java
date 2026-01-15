e893a2e1307bf7cc50e79e69e784941d80e372b1
Diplav Kushwaha <diplavkushwaha@gmail.com>
2026-01-15 21:08:10 +0530

    Create CountPrimes.java

---
+++ CountPrimes.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static int countPrimes(int n) {
+        if (n <= 2) return 0;
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) count++;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int result=countPrimes(num);
+        System.out.println(result);
+    }
+}
